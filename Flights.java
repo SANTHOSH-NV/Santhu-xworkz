d4f8e9fc8e526d8779d3df5522b8b4f5c2b483be
SANTHOSH-NV <santhoshkumarnv.xworkz@gmail.com>
2021-02-10 13:29:57 +0530

    Adding java files of type Arrays

---
+++ Flights.java
@@ -0,0 +1,11 @@
+class Flights
+{
+static String []name={"KingFisher","Deccan","SpiceJet","AirAsia"};
+public static void main(String a[])
+{
+System.out.println(name[0]);
+System.out.println(name[1]);
+System.out.println(name[2]);
+System.out.println(name[3]);
+}
+}
---
+++ Flights1.java
@@ -0,0 +1,11 @@
+class Flights1
+{
+static String[] name={"KingFisher","Deccan","SpiceJet","AirAsia","Indigo"};
+public static void main(String a[])
+{
+for(int i=0;i<=4;i++)
+{
+System.out.println(name[i]);
+}
+}
+}
